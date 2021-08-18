f02b31604f8663ddd81e58409079cdd24af840a3
MaZhijian <18626659853@163.com>
2021-08-18 09:22:32 +0800

    测试将本地工程添加到Git

---
+++ GitTest/src/wintec/mzj/git/FirstClass.java
@@ -0,0 +1,9 @@
+package wintec.mzj.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello Git Project!");
+        System.out.println("Hello Git Project!");
+        System.out.println("Hello Git Project!");
+    }
+}
